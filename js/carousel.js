9b8ce89f706fba5b08200c335c1bc7d9a0527f51
Amity Binkert <amity@binkert.org>
2013-06-18 21:11:43 -0700

    Updated to 2012 theme.  Set a variable for the number of photos to display in the carousel based on screen width

--- js/carousel.js
+++ js/carousel.js
@@ -1,13 +1,39 @@
 // JavaScript Document
 jQuery(document).ready(function($) {
 	
+	$(window).resize(function() {
+		$("#location-gallery").trigger("configuration", ["items.visible", getVisibleItems()]);
+
+	});
+
+
+	/**
+	 * Determine how many images to display in carousel based on device/window with
+	 * @return {integer / number of items to display in carousel}
+	 */
+	function getVisibleItems() {
+
+		var viewport_width = document.documentElement.clientWidth;
+		var device_width = window.screen.width;
+
+		if (viewport_width < 480 || device_width < 480) {
+			return 1;
+		}
+		else if (viewport_width < 768 || device_width < 768) {
+			return 2;
+		}
+		return 3;
+	}
+
+	var visible_items = getVisibleItems();
+
 	$("#location-gallery").carouFredSel({
 		items 	:	{
-			visible: 3,
+			visible: visible_items,
 			minimum: 4
 		},
 		auto: {
-			items	: 3,
+			items	: visible_items,
 			duration: 2800,
 			easing: "swing",
 			delay: 2800
